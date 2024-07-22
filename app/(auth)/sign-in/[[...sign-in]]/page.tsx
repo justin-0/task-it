8ec4da4ff68c43de35c08a0ae35c06aec1744e1e
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-22 14:33:37 +0100

    auth pages styled

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
-    return <SignIn />;
+    return (
+        <div className="flex h-screen flex-col items-center justify-center">
+            <SignIn />
+        </div>
+    );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
-    return <SignUp />;
+    return (
+        <div className="flex h-screen flex-col items-center justify-center">
+            <SignUp />
+        </div>
+    );
 }
