b2cef70305914ea8be2d205507fb8ebd6b69c56a
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-22 14:28:35 +0100

    clerk installed, sign in and sign up page created

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
@@ -19,8 +20,12 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={`${inter.className} min-h-svh`}>{children}</body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} min-h-svh`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
