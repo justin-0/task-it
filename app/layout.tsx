8461e2719c2094679673575be136004d0306b078
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-22 13:40:05 +0100

    homepage / marketing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} min-h-svh`}>{children}</body>
+            <body className={`${inter.className} min-h-svh bg-slate-200`}>
+                {children}
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
-    return <main className=""></main>;
+    return (
+        <div className="mx-6 md:mx-24">
+            <header className="py-4">
+                <nav className="flex items-center justify-between">
+                    <div>
+                        <span className="font-semi text-sm">task-it</span>
+                    </div>
+                    <div>
+                        <Button size="sm" className="md:hidden">
+                            Get started
+                        </Button>
+                    </div>
+                </nav>
+            </header>
+            <main className="flex h-[800px] w-full flex-col items-center justify-center md:mx-auto md:w-[800px]">
+                <h1 className="text-balance text-center text-4xl font-bold">
+                    task-it brings all your tasks, teammates, and tools together
+                </h1>
+                <p className="mt-2 text-pretty text-center text-lg font-light">
+                    Keep everything in the same place-even if your team isn't
+                </p>
+                <Button className="mt-3 hidden md:block" size="lg">
+                    Get started
+                </Button>
+            </main>
+        </div>
+    );
 }
